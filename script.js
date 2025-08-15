7355bd99a13e8035df0ff9fb40f357c9516b5809
JoshuaWX <ejiyooye16571@run.edu.ng>
2025-08-15 03:56:44 +0100

    js and app functionality

--- script.js
+++ script.js
@@ -3,3 +3,55 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */ 
+let meterFeet = document.getElementById("meter-feet")
+let litGal = document.getElementById("lit-gal")
+let kgP = document.getElementById("kg-p")
+let convertInput = document.getElementById("convert-input")
+let convertBtn = document.getElementById("convert-btn")
+
+convertBtn.addEventListener("click", function(){
+    convertToFeet()
+    convertToMeter()
+    convertToGallon()
+    convertToLiters()
+    convertToPounds()
+    convertToKg()
+    btnClicked()
+})
+
+function convertToFeet(){
+    let feetU = Number(convertInput.value )* 3.281
+    let feet = feetU.toFixed(3)
+    meterFeet.textContent = `${convertInput.value} meters = ${feet} feets | `
+}
+function convertToMeter(){
+    let meterU = Number(convertInput.value )/ 3.281
+    let meter = meterU.toFixed(3)
+    meterFeet.textContent += `${convertInput.value} feets = ${meter} meters`
+}
+function convertToGallon(){
+    let gallonU = Number(convertInput.value ) * 0.264
+    let gallon = gallonU.toFixed(3)
+    litGal.textContent = `${convertInput.value} liters = ${gallon} gallons | `
+}
+function convertToLiters(){
+    let literU = Number(convertInput.value ) / 0.264
+    let liter = literU.toFixed(3)
+    litGal.textContent += `${convertInput.value} gallons = ${liter} liters`
+}
+function convertToPounds(){
+    let poundU = Number(convertInput.value ) * 2.204
+    let pound = poundU.toFixed(3)
+    kgP.textContent = `${convertInput.value} kilograms = ${pound} pounds | `
+}
+function convertToKg(){
+    let kgU = Number(convertInput.value ) / 2.204
+    let kg = kgU.toFixed(3)
+    kgP.textContent += `${convertInput.value} pounds = ${kg} kilograms`
+}
+function btnClicked(){
+    convertInput.value = ""
+    alert("Converting Values...")
+}
+
+
